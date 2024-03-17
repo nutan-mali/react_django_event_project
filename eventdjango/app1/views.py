e126834b103774c7c9a0d18a1ac15bc86733cad9
nutan-mali <nutansm123@gmail.com>
2024-03-17 23:01:56 +0530

    add requirments.txt

--- eventdjango/app1/views.py
+++ eventdjango/app1/views.py
@@ -4,34 +4,24 @@ from .serializers import EventSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-# from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
+from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
 # from .permissions import IsOwnerOnly
 # from rest_framework.permissions import IsAuthenticated
 # from rest_framework import permissions
 # from rest_framework.permissions import AllowAny
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def event(request):
-    queryset  = Event.objects.all()
-    print(queryset )
-    serializer = EventSerializer(queryset , many=True)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def event_create(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        queryset  = Event.objects.all()
+        print(queryset )
+        serializer = EventSerializer(queryset , many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-    else:
-        return Response({'detail': 'Method not allowed'}, status=405)
-# @api_view(['POST'])
-# def event_create(request):
-#     serializer = EventSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=201)  # Return a successful response
-#     return Response(serializer.errors, status=400)  # Return error response if data is not valid
+            print(queryset )
+            return Response(serializer.data, status=201)  # Created
+        return Response(serializer.errors, status=400)  # Bad Request
--- eventreact/src/App.js
+++ eventreact/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [events, setEvents] = useState([]);
@@ -18,88 +21,36 @@ function App() {
     getAllEvents();
   }, []);
 
+  const handleLikeClick = (event, eventId) => {
+    event.preventDefault();
+    const updatedEvents = events.map(evt =>
+      evt.id === eventId ? { ...evt, isLiked: !evt.isLiked } : evt
+    );
+    setEvents(updatedEvents);
+  };
+  
+
   return (
     <div className='App'>
       <h1>Event List</h1>
-     {events.map(event => (
-        <div>
-        <h1>{event.event_name}</h1>
-        <p>Date: {event.date}</p>
-        <p>Time: {event.time}</p>
-        <p>Location: {event.location}</p>
-        <img src={event.image} alt="Event" />
-        {event.is_liked ? (
-          <img src="path_to_redHeart.svg" alt="Liked" className="heartIcon" />
-        ) : (
-          <img src="path_to_whiteHeart.svg" alt="Not Liked" className="heartIcon" />
-        )}
-      </div>
-      ))} 
+      {events.map(event => (
+        <div key={event.id}>
+          <h3>{event.event_name}</h3>
+          <p>Date: {event.date}</p>
+          <p>Time: {event.time}</p>
+          <p>Location: {event.location}</p>
+          <img src={event.image} alt="Event" />
+          <FontAwesomeIcon
+            icon={event.isLiked ? fasHeart : faHeart}
+            style={{ color: event.isLiked ? 'red' : 'black' }}
+            onClick={(e) => handleLikeClick(e, event.id)}
+          />
+
+
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Login from './MyComponent/Login'; // Update the import path if necessary
-
-// function App() {
-//   const [events, setEvents] = useState([]);
-
-//   useEffect(() => {
-//     async function getAllEvents() {
-//       try {
-//         const response = await axios.get('http://127.0.0.1:8000/api/event/');
-//         console.log(response.data); // Optional: Log the response to check data
-//         setEvents(response.data);
-//       } catch (error) {
-//         console.error('Error fetching events:', error);
-//       }
-//     }
-
-//     getAllEvents();
-//   }, []);
-
-//   return (
-//     <div className='App'>
-//       <h1>Event List</h1>
-//       {events.map(event => (
-//         <div key={event.id}>
-//           <h3>{event.event_name}</h3>
-//           <p>Date: {event.date}</p>
-//           <p>Time: {event.time}</p>
-//           <p>Location: {event.location}</p>
-//           {/* Display other event details as needed */}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// const MainApp = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         {/* Other routes */}
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default MainApp;
